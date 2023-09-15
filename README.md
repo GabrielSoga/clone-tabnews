e4efe4e1f16157c1b826b73be36bce877090284a
Gabriel Soga <gabrielfsoga@gmail.com>
2023-09-15 14:49:13 +0000

    chores: adding prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Reimplementation of Tabnews for practice purposes
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-  return <iframe width="1024" height="768" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=yp7wxR2G0z7uopPw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  return (
+    <iframe
+      width="1024"
+      height="768"
+      src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=yp7wxR2G0z7uopPw"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  );
 }
 
 export default Home;
